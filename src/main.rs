387585f1820e1890c5857861f633ea324344a18f
Jash Desai <jashdesai27@gmail.com>
2025-05-26 17:54:01 -0500

    added lexer with multi-digit number support

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,30 @@ enum Token{
 }
 
 fn main() {
-    let input = "5 + 3";
+    let input = "42 + 378";
+    let chars: Vec<char> = input.chars().collect();
     let mut tokens = Vec::new();
+    let mut i = 0;
 
-    for ch in input.chars() {
+    while i < chars.len() {
+        let ch = chars[i];
+
         if ch == ' ' {
+            i += 1;
             continue;
         } else if ch == '+' {
-            tokens.push(Token::Plus);
-        } else if ch.is_ascii_digit(){
-            let digit = ch.to_digit(10).unwrap() as i64;
-            tokens.push(Token::Number(digit));
+            tokens.push(Token:: Plus);
+            i += 1;
+        } else if ch.is_ascii_digit() {
+            let start = i;
+            while i < chars.len() && chars[i].is_ascii_digit() {
+                i += 1;
             }
+            let num_str: String = chars[start..i].iter().collect();
+            let num = num_str.parse::<i64>().unwrap();
+            tokens.push(Token::Number(num));
         }
+    }
 
     tokens.push(Token::Eof);
     println!("{:?}", tokens);
