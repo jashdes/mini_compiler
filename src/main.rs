dd2468080de97a25a2a8b2f2c74b56b7992606fa
Jash Desai <jashdesai27@gmail.com>
2025-05-26 17:23:42 -0500

    implemented basic tokenization

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#derive[(Debug, PartialEq)]
+#[derive(Debug, PartialEq)]
 enum Token{
     Number(i64),
     Plus,
@@ -7,5 +7,19 @@ enum Token{
 
 fn main() {
     let input = "5 + 3";
-    println!("Input: {}", input);
+    let mut tokens = Vec::new();
+
+    for ch in input.chars() {
+        if ch == ' ' {
+            continue;
+        } else if ch == '+' {
+            tokens.push(Token::Plus);
+        } else if ch.is_ascii_digit(){
+            let digit = ch.to_digit(10).unwrap() as i64;
+            tokens.push(Token::Number(digit));
+            }
+        }
+
+    tokens.push(Token::Eof);
+    println!("{:?}", tokens);
 }
